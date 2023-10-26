102d7b438abd890a6b2549858ac49e6c0800fef1
Franco Araya Leiva <onecalledfrank@gmail.com>
2023-10-26 18:17:39 -0300

    Day 1 - Python Challenge

---
+++ Day 1/main.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Hola, bienvenido a mi programa!")
+name = input("¿En qué ciudad creciste?:\n")
+mascot = input("¿Cuál era el nombre de tu mascota?:\n")
+print('El nombre de tu banda es:\n' + name + " " + mascot)
+print("Espero te haya gustado!")
